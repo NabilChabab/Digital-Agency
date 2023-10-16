15e79add802c9691e59eabd929cb7ae765191de3
nabilchabab <nabil.chababnabil@gmail.com>
2023-10-16 15:07:38 +0100

    DigitalAgency

--- script.js
+++ script.js
@@ -2,14 +2,22 @@ var email = document.getElementById("email");
 var pass = document.getElementById("pass");
 var btn = document.getElementById("btnlog");
 
-btn.onclick = () => {
-    if(email.value == "" || pass.value == ""){
-        alert("Please Fill all inputs !");
+
+btn.addEventListener("click", () =>{
+
+    const emailValue = "nabil.chabab@gmail.com";
+    const passValue = "123456";
+
+    // console.log("123456" === pass.value);
+    if( emailValue ===  email.value && passValue === pass.value) {
+        alert("Welcoime!!");
+        window.location.href="../index.html"
     }
-    else if(email.value == "nabil.chabab@gmail.com" && pass.value == "123456"){
-        alert("Welcome!!!");
+    else if(email.value === "" || pass.value === "" ){
+        alert("fill all inputs");
     }
     else{
-        alert("Ghayerha");
+        alert("user not found");
     }
-};
+});
+
