f929a417b27be195394f75585d75d39cd8566e12
nabilchabab <nabil.chababnabil@gmail.com>
2023-10-21 17:34:43 +0100

    Adding FAQ Page

---
+++ assets/js/faq.js
@@ -0,0 +1,12 @@
+const questions = document.querySelectorAll("#arrow");
+const answers = document.querySelectorAll(".answer");
+
+questions.forEach((question, index) => {
+    question.addEventListener("click", () => {
+        if (answers[index].style.display === "block") {
+            answers[index].style.display = "none";
+        } else {
+            answers[index].style.display = "block";
+        }
+    });
+});
